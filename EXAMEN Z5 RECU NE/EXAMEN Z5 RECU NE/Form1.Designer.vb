1647a0769710022b47a85f4a3b41133c37fc5143
myauxcode <104797580+myauxcode@users.noreply.github.com>
2022-07-22 18:59:39 -0400

    Add files via upload

---
+++ EXAMEN Z5 RECU NE/EXAMEN Z5 RECU NE/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.EXAMENToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Ejercicio1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Ejercicio2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(363, 134)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(363, 239)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EXAMENToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(632, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'EXAMENToolStripMenuItem
+        '
+        Me.EXAMENToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Ejercicio1ToolStripMenuItem, Me.Ejercicio2ToolStripMenuItem})
+        Me.EXAMENToolStripMenuItem.Name = "EXAMENToolStripMenuItem"
+        Me.EXAMENToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.EXAMENToolStripMenuItem.Text = "EXAMEN"
+        '
+        'Ejercicio1ToolStripMenuItem
+        '
+        Me.Ejercicio1ToolStripMenuItem.Name = "Ejercicio1ToolStripMenuItem"
+        Me.Ejercicio1ToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.Ejercicio1ToolStripMenuItem.Text = "Ejercicio 1"
+        '
+        'Ejercicio2ToolStripMenuItem
+        '
+        Me.Ejercicio2ToolStripMenuItem.Name = "Ejercicio2ToolStripMenuItem"
+        Me.Ejercicio2ToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.Ejercicio2ToolStripMenuItem.Text = "Ejercicio 2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(632, 420)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents EXAMENToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Ejercicio1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Ejercicio2ToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ EXAMEN Z5 RECU NE/EXAMEN Z5 RECU NE/Form1.vb
@@ -0,0 +1,130 @@
+﻿Public Class Form1
+
+    'ejercicio 1
+
+    Public Function ejercicio1(n As Integer) As Integer
+
+        Dim nn, e, d As Integer
+
+        While n > 0
+
+            d = n Mod 10
+
+            nn = nn + d * (10 ^ e)
+
+            e = e + 1
+
+            n = Elim_todos_num(n, d)
+
+        End While
+        Return nn
+
+    End Function
+
+    Public Function Elim_todos_num(n As Integer, num As Integer) As Integer
+        Dim nd, d, nn, e As Integer
+
+        nd = Cant_digs(num)
+        d = 0
+        nn = 0
+        e = 0
+
+        While n > 0
+            d = n Mod (10 ^ nd)
+            n = n \ (10 ^ nd)
+
+            If d <> num Then
+
+                nn = nn + (d * (10 ^ e))
+                e = e + nd
+
+            End If
+        End While
+
+        Return nn
+    End Function
+
+    Public Function Cant_digs(n As Integer) As Integer
+        Dim cd, d As Integer
+        cd = 0
+        If n = 0 Then
+            n = 1
+        End If
+        While n > 0
+            d = n Mod 10
+            n = n \ 10
+            cd = cd + 1
+        End While
+        Return cd
+    End Function
+
+    Private Sub Ejercicio1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Ejercicio1ToolStripMenuItem.Click
+        TextBox2.Text = ejercicio1(TextBox1.Text)
+    End Sub
+
+
+    'ejercicio 2
+
+    Public Function Ejercicio2(n As Integer) As Integer
+        Dim may, men, nr As Integer
+
+        men = Dig_men(n)
+        may = Dig_may(n)
+
+        nr = Unir2NE(men, may)
+
+        Return nr
+
+
+    End Function
+
+    Public Function Unir2NE(n As Integer, num As Integer) As Integer
+        Dim nn As Integer
+        nn = 0
+        nn = n * 10 ^ (Cant_digs(num)) + num
+        Return nn
+
+    End Function
+
+
+
+    Public Function Dig_may(n As Integer) As Integer
+        Dim d1, d2 As Integer
+
+        d1 = n Mod 10
+        n = n \ 10
+
+        While n > 0
+            d2 = n Mod 10
+            n = n \ 10
+
+            If d2 > d1 Then
+                d1 = d2
+            End If
+        End While
+        Return d1
+
+    End Function
+
+
+    Public Function Dig_men(n As Integer) As Integer
+        Dim dm, d As Integer
+        dm = n Mod 10
+        n = n \ 10
+
+        While n > 0
+            d = n Mod 10
+            n = n \ 10
+
+            If d < dm Then
+                dm = d
+            End If
+        End While
+        Return dm
+
+    End Function
+
+    Private Sub Ejercicio2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Ejercicio2ToolStripMenuItem.Click
+        TextBox2.Text = Ejercicio2(TextBox1.Text)
+    End Sub
+End Class
